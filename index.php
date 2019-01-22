cd02ea26bbf659a9d132f584e0bff1a7a99c6802
Squalli <virgile.gibello@gmail.com>
2019-01-22 09:35:21 +0100

    Création du fichier index.php

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="./style.css"/>
+    <title>Trombino</title>
+   
+  </head>
+  <body>
+    
+    <!-- jQuery -->
+    <script
+          src="https://code.jquery.com/jquery-3.3.1.min.js"
+          integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+          crossorigin="anonymous">
+    </script>
+    <script>
+      
+       
+    </script>
+  </body>
+</html>
